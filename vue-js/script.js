85a5cf0befdea06ce41983748bb779092a3f6146
hundredvisionsguy <cwinikka@gmail.com>
2020-03-05 22:38:59 -0800

    adding VueJS solution

---
+++ vue-js/script.js
@@ -0,0 +1,56 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello SuperQuest!'
+  }
+})
+var app2 = new Vue({
+	el: "#app2",
+	data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+var app3 = new Vue({
+	el: "#app3",
+	data: {
+		todos: [
+			{ text: "this"},
+			{ text: "that"},
+			{ text: "the other thing" }
+		]
+	}
+})
+var app4 = new Vue({
+	el: "#app4",
+	data: {
+		message: "Hello, SuperQuest"
+	},
+	methods: {
+		reverseMessage: function() {
+			this.message = this.message.split('').reverse().join('')
+		}
+	}
+})
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+// Vue Component
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
